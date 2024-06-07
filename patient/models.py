f39214a60cf9bf6d6dfdb45b26ab0dbdd76d6080
Ajay Thakur <ajaythk.94@gmail.com>
2024-06-07 09:32:12 +0545

    models_created

--- patient/models.py
+++ patient/models.py
@@ -1,3 +1,54 @@
+from datetime import date
+
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    extras = models.JSONField(default=dict)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class PatientDetail(BaseModel):
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    gender = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=100, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+
+    @property
+    def age(self):
+        if self.date_of_birth:
+            today = date.today()
+            age = today.year - self.date_of_birth.year - ((today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day))
+            return age
+        return None
+
+    class Meta:
+        db_table = 'patient'
+        verbose_name = 'Patient'
+        verbose_name_plural = 'Patients'
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.full_name
+
+
+class Assessment(BaseModel):
+    patient = models.ForeignKey(PatientDetail, on_delete=models.CASCADE)
+    assessment_type = models.CharField(max_length=100, null=True, blank=True)
+    assessment_date = models.DateField(null=True, blank=True)
+    questions_answers = models.CharField(max_length=100, null=True, blank=True)
+    final_score = models.FloatField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'assessment'
+        verbose_name = 'Assessment'
+        verbose_name_plural = 'Assessments'
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.assessment_type
